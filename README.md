4800bd7a54c9ccd4e0e505ce7041da0a3f7aa8be
XFTY <StevenXue1@outlook.com>
2023-10-06 13:58:49 +0800

    Update socket

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MoveToHere
-#### <font color=lightseagreen> 如果你想了解自述文件的原始信息，请访问中文文档。 </font>  
-#### <font color=lightseagreen>If you want to know the original information of the readme file, please visit the Chinese documentation.</font>  
+#### 如果你想了解自述文件的原始信息，请访问中文文档。
+#### If you want to know the original information of the readme file, please visit the Chinese documentation. 
 ## Introduction
 FTPReader is a **free** and **open source** for automatic server file  transfers
 Project, which can be used by users who need to automatically transfer files on a regular basis.
--- server/moveToHere.py
+++ server/moveToHere.py
@@ -1,6 +1,118 @@
 import os
 import json
+import sys
 import time
+import socket
+import cryptocode
+
+# 对日志系统进行基础配置
+"""logging.basicConfig(
+    filename="latest.log",
+    filemode="w",
+    format="[%(asctime)s/%(levelname)s]%(message)s",
+    datefmt="%d-%M-%Y %H:%M:%S",
+    level=logging.INFO
+)"""
+
+
+# 定义日志类
+class logging:
+    def __init__(self):
+        with open("latest.log", "a", encoding="utf-8") as f:
+            f.write(
+                "\n==========[Start in {}]==========\n".format(time.strftime("%Y/%m/%d %H:%m:%S", time.localtime())))
+
+    # 写入文件并打印，私有函数
+    def write(self, level, text):
+        with open("latest.log", "a", encoding="utf-8") as f:
+            log = "[{}/{}] {}".format(time.strftime("%Y/%m/%d %H:%m:%S", time.localtime()), level, text)
+            f.write(log + "\n")
+            print(log)
+
+    def close(self):
+        with open("latest.log", "a", encoding="utf-8") as f:
+            f.write(
+                "==========[End in {}]============\n\n\n".format(time.strftime("%Y/%m/%d %H:%m:%S", time.localtime())))
+
+    # 类的接口
+    def debug(self, text: str = None):
+        self.write("DEBUG", text)
+
+    def info(self, text=None):
+        self.write("INFO", text)
+
+    def warning(self, text: str = None):
+        self.write("WARNING", text)
+
+    def error(self, text: str = None):
+        self.write("ERROR", text)
+
+    def failed(self, text: str = None):
+        self.write("FAILED", text)
+
+
+# 主程序
+class TCPServer:
+    def __init__(self):
+        self.logging = logging()
+        # 读取配置文件并赋值给self.config
+        # 先尝试读取文件，如果文件不存在则终止程序运行
+        self.logging.info("Loading config file")
+        try:
+            with open("config.json", "r", encoding="utf-8") as f:
+                self.config = json.loads(f.read())
+            self.logging.info("config file load successfully!")
+        except Exception as e:
+            self.logging.error("{}".format(e))
+            self.logging.info("stopping server ...")
+            sys.exit()
+
+        self.logging.info("Starting socket ...")
+
+        try:
+            # 创建socket连接并监听由配置文件提供的IP和端口
+            # 当然，这和FIB和IAA没有关系，(玩5玩的)[doge]
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # self.server.bind(("127.0.0.1",25565))
+            self.server.bind((self.config["ipconfig"]["server-ip"], int(self.config["ipconfig"]["server-port"])))
+            self.server.listen(self.config["max_connect"])  # 监听，参数是最大连接数
+        except Exception as e:
+            self.logging.error("An error occurred that prevented the server from starting:")
+            self.logging.error(e)
+            self.logging.warning("Failed to start server! Have you set up your IP and port correctly?")
+            self.close()
+
+        # 服务器主代码
+        # 无限循环，接受客户端的请求
+        while True:
+            # 接受客户端请求
+            self.logging.info("done! waiting for client(s)")
+            self.client, self.address = self.server.accept()
+            # 存储获取的数据
+            self.recv_data = self.client.recv(1024)
+            self.recv_data.decode("utf-8")
+
+    def testConnection(self):
+        self.client.send(f"Welcome to connect {self.config['serverBasicConfig']['server-name']} server!".encode("utf-8"))
+
+    def sendTextData(self, text):
+        if self.config["serverBasicConfig"]["encryption-mode"]["enable"]:
+            text = cryptocode.encrypt(text, self.config["serverBasicConfig"]["encryption-mode"]["key"])
+            text = text.encode("utf-8")
+        else:
+            text.encode("utf-8")
+        self.client.send(text)
+
+    def close(self, code: str | int = -1):
+        self.logging.info("stopping server ...")
+        self.logging.close()
+        sys.exit(code)
+
+    def safeclose(self, code=0):
+        self.logging.info("stopping server ...")
+        self.server.close()
+        self.logging.close()
+        sys.exit(code)
 
 
 class setup:
@@ -56,7 +168,6 @@ Please wait a few seconds, we are setting up something ...
             else:
                 pass
 
-
     def setcomplete(self):
         os.system("cls")
         input(
@@ -93,4 +204,4 @@ Please press enter to continue
 
 
 if __name__ == "__main__":
-    setup()
+    TCPServer()
